24aa4e09f9aa51a5aec211032cbc05547fb6bc4f
Laura Fabian <62635340+llaufabian@users.noreply.github.com>
2024-08-28 17:22:15 -0300

    Add files via upload

---
+++ 01-aula01.py
@@ -0,0 +1,20 @@
+# Comentários
+# Continuar na linha de baixo
+print("Laura") # Comentário na linha do código
+print('Laura')
+print('Laura "A Honrada" Fabian')
+print("Laura 'A Honrada' Fabian")
+print("Laura \"A Honrada\" Fabian")
+
+teste = "oi"
+print(teste)
+print(type(teste))
+teste = 17
+print(teste)
+print(type(teste))
+teste = 12.1
+print(teste)
+print(type(teste))
+teste = False
+print(teste)
+print(type(teste))
