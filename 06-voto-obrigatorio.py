1c7f91ffcc56d2ff25d7a56684dbc909a4c850f0
Laura Fabian <62635340+llaufabian@users.noreply.github.com>
2024-09-12 17:05:57 -0300

    Add files via upload

---
+++ 06-voto-obrigatorio.py
@@ -0,0 +1,11 @@
+# Atividade - Implementar o algoritmo do voto obrigatório em Python, de acordo com o pseudo-código em portugol descrito em sala.
+
+nome = str(input("Digite o seu nome: "))
+idade = int(input("Olá! Digite a sua idade: "))
+
+if idade >=18 and idade < 65:
+    print(f"{nome}, seu voto é obrigatório!")
+elif idade >= 16 and idade <=17 or idade >= 65:
+    print(f"{nome}, seu voto é opcional!")
+else:
+    print(f"{nome}, seu voto ainda não é permitido!")
