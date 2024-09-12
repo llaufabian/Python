a4f59ca666aaad040b00e4955cacc9084504d717
Laura Fabian <62635340+llaufabian@users.noreply.github.com>
2024-09-12 17:09:53 -0300

    Delete 00-atribuicao-de-variaveis.py

--- 00-atribuicao-de-variaveis.py
+++
@@ -1,9 +0,0 @@
-# atribuição de variáveis
-nome = "Laura"
-idade = 16
-altura = 1.60
-
-# exibir mensagem na tela
-print(" nnome: ", nome, " idade: ", idade, " altura: ", altura)
-# formatação com f-string
-print(f"nome: {nome} idade: {idade} altura: {altura}")
