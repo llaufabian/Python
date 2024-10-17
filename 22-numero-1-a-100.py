ba2f86fb92b3d7caac60ba2d66f883f236dd15b9
Laura Fabian <62635340+llaufabian@users.noreply.github.com>
2024-10-17 18:14:58 -0300

    Update 22-numero-1-a-100.py
    Inclusão dos autores

--- 22-numero-1-a-100.py
+++ 22-numero-1-a-100.py
@@ -1,4 +1,4 @@
-# Atividade - Algoritmo "1 a 100"
+# Atividade - Algoritmo "1 a 100" - Laura Fabian, Gabrielly O., Thayná A.
 # Esse código irá exibir os números inclusos entre 1 e 100.
 
 # Início do laço de repetição.
