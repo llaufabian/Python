2d21323dd78b83a6b071b6b05715d1f6236b0671
Laura Fabian <62635340+llaufabian@users.noreply.github.com>
2024-11-07 15:15:45 -0300

    Add files via upload

---
+++ 22-lista-numeros-inteiros.py
@@ -0,0 +1,16 @@
+
+minhalista = []
+
+for i in range(5):
+    numero = int(input(f"Digite o {i+1}º número inteiro: \n"))
+    minhalista.append (numero)
+
+menor = min(minhalista)
+maior = max(minhalista)
+soma = sum(minhalista)
+print(f"\nO menor num. da lista: {menor}\n")
+print(f"O maior num. da lista: {maior}\n") 
+print(f"A soma dos num. da lista: {soma}\n")
+
+for lista in minhalista:
+    print(lista)
